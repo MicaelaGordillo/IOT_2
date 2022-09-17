9a4524a8100bf1bce170b48ed4bd3ef279485fe8
YamilBarrientos <113692652+YamilBarrientos@users.noreply.github.com>
2022-09-16 21:47:56 -0400

    Update EjemploST_3.ino

--- EjemploST_3/EjemploST_3.ino
+++ EjemploST_3/EjemploST_3.ino
@@ -77,7 +77,8 @@ void loop(){
   client.print("</style>");  
   client.print("<div style='width: 100%; height: 10px; background-color: #164275;'></div>");
   client.print("<div style='width: 100%; height: 200px; display:inline-flex; margin-top: 10px; margin-bottom: 10px;'>");
-  client.print("<div style='margin-top: 15px; margin-left: 30px; margin-bottom: 15px;'><img src='https://www.unir.net/wp-content/uploads/2021/02/shutterstock_412165618.jpg' style='display:block; margin:auto' width='20%'></div>");
+  client.print("<div style='margin-top: 15px; margin-left: 30px; margin-bottom: 15px;'><img src='https://ucblpz.com/wp-content/uploads/2018/12/UCB-logo-lapaz.png'></div>");
+  client.print("<div style='text-align: right; margin-left: 150px; margin-top: 50px;'><h1 style='color: #164275; font-size: 40px;'>Internet de las cosas</h1></div>");
   client.print("</div>");
   client.print("<div style='width: 100%; height: 10px; background-color: #164275;'></div>");
   client.print("<center>");
